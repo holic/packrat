b0b2b70faf89e3c9cbf37d7d0a28b7b08d8e6a56
Josh Pigford <josh@joshpigford.com>
2021-11-16 12:32:36 -0600

    current_wallet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  def current_wallet
+    return unless session[:wallet]
+    @current_wallet ||= Wallet.find_or_create_by(address: session[:wallet])
+  end
+
 end
