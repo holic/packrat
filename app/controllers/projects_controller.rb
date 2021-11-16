0c1844cafb565a8474b024810e48560a5aefae87
Josh Pigford <josh@joshpigford.com>
2021-11-16 15:12:25 -0600

    Basic admin bits

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,6 +8,10 @@ class ProjectsController < ApplicationController
   def show
   end
 
+  def new
+    @project = Project.new  
+  end
+
   def follow
     if @current_wallet.follows.find_or_create_by(project: @project) 
       redirect_to project_path(@project.slug)
